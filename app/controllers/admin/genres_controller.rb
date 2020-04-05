6a5d38e324592b2ec4d2c26d7936e6e8c496d938
mushakora <mushakora89br@yahoo.co.jp>
2020-04-05 19:27:44 +0900

    [add]views[genre,item]controller[genre,item]

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -11,11 +11,17 @@ class Admin::GenresController < ApplicationController
   def create
   	  @genre = Genre.new(genre_params)
   	  @genre.save
+      redirect_to edit_admin_genre_path(@genre)
   end
 
   def update
   	  @genre = Genre.find(params[:id])
   	  @genre.update(genre_params)
-  	  redirct_to genres_path
+  	  redirect_to admin_genres_path
   end
+
+  private
+    def genre_params
+        params.require(:genre).permit(:genre_name, :genre_status)
+    end
 end
