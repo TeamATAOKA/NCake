2bb76b8221f1547db688f2941cb4d266281c03f3
matuyama1223 <matu1223@icloud.com>
2020-04-08 18:43:51 +0900

    [fix]cartitem

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :cart_item
+  has_many :cart_items
   has_many :order
   has_many :post
 end
