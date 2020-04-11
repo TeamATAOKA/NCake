77cb9c16a6bdc125cda2b9b58fa0dedd8310d9d7
田中良征 <tanakaryousei@tanakayoshimasanoMacBook-Pro.local>
2020-04-11 17:15:02 +0900

    [Fix]注文確認画面

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,10 +1,10 @@
 class OrdersController < ApplicationController
+  before_action :confirm_params, only: [:confirm]
   def index
       @orders = Order.all
   end
 
   def show
-      @order = Order.find(params[:id])
   end
 
   def new
@@ -26,11 +26,14 @@ class OrdersController < ApplicationController
   def confirm
     @user = current_user
     @post = Post.new
-    @oreder = Order.find(params[:id])
   end
 
   def update
       @order = Order.find(params[:id])
       @order.update
+  end
+private
+  def confirm_params
+   @order = Order.new(params.require(:order).permit(:name, :user_id, :address, :payment, :postage))
   end
 end
