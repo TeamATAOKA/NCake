515258b107108e17f890052e2fca7f723e9f4b41
mushakora <mushakora89br@yahoo.co.jp>
2020-04-12 21:00:25 +0900

    [add]注文情報の確認、登録

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,5 @@
 class OrdersController < ApplicationController
-  before_action :confirm_params, only: [:confirm]
+
   def index
       @orders = Order.all
   end
@@ -10,31 +10,55 @@ class OrdersController < ApplicationController
   def new
       @order = Order.new
       @user = current_user
-      @posts = Post.all
+      @posts = @user.posts
   end
 
   def create
-      @order = Order.build(order_params)
-      respond_to do |format|
-        if @order.save
-           @post =Post.new
-           format.html (redirect_to home_thanks_path)
-        else
-           format.html (render action: 'new')
+      session[:order] = Order.new
+      session[:payments] = params[:payment_option]
+      session[:addresses] = params[:address_option]
+      session[:order][:user_id] = current_user
+        if session[:payments].to_i == 1
+         session[:order][:payment] = "クレジットカード"
+        elsif session[:payments].to_i == 2
+            session[:order][:payment] = "銀行振込"
         end
-      end
+
+        if session[:addresses].to_i == 1
+           session[:order][:name] = current_user.name
+           session[:order][:postcode] = current_user.postcode
+           session[:order][:address] = current_user.address
+
+        elsif   session[:addresses].to_i == 2
+                post = Post.find(params[:id])
+                session[:order][:name] = post.name
+                session[:order][:postcode] = post.postcode
+                session[:order][:address] =  post.address
+
+        elsif   session[:addresses].to_i == 3
+                session[:order][:name] = params[:new_name]
+                session[:order][:postcode] = params[:new_postcode]
+                session[:order][:address] = params[:new_address]
+        end
+      redirect_to confirm_orders_path
   end
 
   def confirm
-    @user = current_user
+      @user = current_user
+      @cart_item = @user.cart_items
+      session[:order][:postage] = 800
   end
 
   def update
       @order = Order.find(params[:id])
       @order.update
   end
-private
-  def confirm_params
-   @order = Order.new(params.require(:order).permit(:postcode, :name, :user_id, :address, :payment, :postage))
+
+  def done
+      order = Order.new(session[:order])
+      order.user_id = current_user.id
+      order.save
   end
+
+
 end
