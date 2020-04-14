02093db46da4bdd2e1ff0a82599ca51cdb43e589
mushakora <mushakora89br@yahoo.co.jp>
2020-04-14 21:09:53 +0900

    [add]注文件数表示

--- app/controllers/admin/home_controller.rb
+++ app/controllers/admin/home_controller.rb
@@ -3,5 +3,7 @@ class Admin::HomeController < ApplicationController
   end
 
   def top
+  	  range = Date.today.beginning_of_day..Date.today.end_of_day
+  	  @orders = Order.where(created_at: range)
   end
 end
