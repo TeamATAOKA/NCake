324cd7f67e858236da4a65ad7b9b9eb7db0ed664
YasukiArashiro <yasuki.arashiro1108@gmail.com>
2020-04-14 11:20:09 +0000

    [Add]アクセス制限機能

--- app/controllers/admin/order_items_controller.rb
+++ app/controllers/admin/order_items_controller.rb
@@ -1,6 +1,6 @@
 class Admin::OrderItemsController < ApplicationController
+	before_action :admin_user
 	def update
-		@order = Order.find(params[:id])
 		@order_item = OrderItem.find(params[:id])
 		@order_item.update(order_item_params)
 		redirect_to admin_order_path(@order_item.order.id)
@@ -9,5 +9,11 @@ class Admin::OrderItemsController < ApplicationController
     private
     def order_item_params
       params.require(:order_item).permit(:order_id, :item_id, :item_count, :sale_price, :production_status)
+    end
+
+    def admin_user
+      unless admin_signed_in?
+        redirect_to root_path
+      end
     end
 end
--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,5 @@
 class Admin::UsersController < ApplicationController
+  before_action :admin_user
   def index
   	  @users = User.page(params[:page]).per(5)
   end
@@ -20,5 +21,11 @@ class Admin::UsersController < ApplicationController
   private
     def user_params
         params.require(:user).permit(:first_name, :last_name, :first_name_kana, :last_name_kana, :postcode, :address, :phone_number, :email)
+    end
+
+    def admin_user
+      unless admin_signed_in?
+        redirect_to root_path
+      end
     end
 end
